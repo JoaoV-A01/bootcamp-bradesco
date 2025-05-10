ff0014714e1a13dad797b880614fc5b765a7b470
JoaoV-A01 <victor.joao01101010@gmail.com>
2025-05-10 19:32:39 -0300

    ajustando CORS

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -3,11 +3,19 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/// Initializes our RESTful API.
+///
+/// The [OpenAPIDefinition] annotation was used to enable HTTPS in the Swagger UI.
+/// For more details, see the following post on Stack Overflow:
+/// <a href="https://stackoverflow.com/a/71132608/3072570">...</a>
+///
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
